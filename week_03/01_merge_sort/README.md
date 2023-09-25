a373072e15ac59003fccee4adef5168321c98f89
Rohit Joshi <rohit.joshiadvanced@gmail.com>
2023-09-25 11:36:21 +0530

    Week 3 merge sort

---
+++ week_03/01_merge_sort/README.md
@@ -0,0 +1,8 @@
+# Week 3 .
+
+### Title: Merge sort
+
+### Problem statement
+Write a program in Ruby that implements merge sort algorithm
+
+### Comments/Description/Learnings
---
+++ week_03/01_merge_sort/solution.rb
@@ -0,0 +1,39 @@
+def merge_sort(array)
+  if array.length > 1
+    mid = array.length / 2
+    puts "left"
+    p left = array[0...mid]
+    puts "right"
+    p right = array[mid..(array.length - 1)]
+    puts ""
+    merge_sort(left)
+    merge_sort(right)
+
+    i = j = k = 0
+
+    while i < left.length && j < right.length
+      if left[i] <= right[j]
+        array[k] = left[i]
+        i += 1
+      else
+        array[k] = right[j]
+        j += 1
+      end
+      k += 1
+    end
+
+    if i < left.length
+      array[k] = left[i]
+      i += 1
+      k += 1
+    end
+
+    if j < right.length
+      array[k] = right[j]
+      j += 1
+      k += 1
+    end
+  end
+  array
+end
+puts merge_sort([21, 43, 73, 8, 15]).inspect
