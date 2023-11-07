d421a0f2d6b70819cfc350d774441a6d568f01d7
Rohit Joshi <rohit.joshiadvanced@gmail.com>
2023-11-07 17:29:29 +0530

    Add test cases for natural sorting

--- week_05/05_natural_sorting/test.rb
+++ week_05/05_natural_sorting/test.rb
@@ -5,4 +5,34 @@ class Test < Minitest::Test
   def setup
     @solution = Solution.new
   end
+
+  def test_unsorted_array
+    input = %w(item1 item12 item7 item2 item3 item4 item10 item11)
+    expected_output = %w(item1 item2 item3 item4 item7 item10 item11 item12)
+    assert_equal expected_output, @solution.call(input)
+  end
+
+  def test_sorted_array
+    input = %w(item1 item2 item3 item4 item7 item10 item11 item12)
+    expected_output = %w(item1 item2 item3 item4 item7 item10 item11 item12)
+    assert_equal expected_output, @solution.call(input)
+  end
+
+  def test_empty_array
+    input = []
+    expected_output = []
+    assert_equal expected_output, @solution.call(input)
+  end
+
+  def test_one_element
+    input = ["item1"]
+    expected_output = ["item1"]
+    assert_equal expected_output, @solution.call(input)
+  end
+
+  def test_reversed_sorted_array
+    input = %w(item12 item11 item10 item7 item4 item3 item2 item1)
+    expected_output = %w(item1 item2 item3 item4 item7 item10 item11 item12)
+    assert_equal expected_output, @solution.call(input)
+  end
 end
