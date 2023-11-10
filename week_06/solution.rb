a94f431f03d6cca459ee49f64a5e0a5c30fa857f
Rohit Joshi <rohit.joshiadvanced@gmail.com>
2023-11-10 15:02:37 +0530

    Rename methods

--- week_06/solution.rb
+++ week_06/solution.rb
@@ -2,7 +2,7 @@ require_relative "node"
 require_relative "min_heap"
 
 class Solution
-  def encode(input)
+  def build_huffman_tree(input)
     nodes = []
     input.each_char.tally.each do |symbol, freq|
       nodes << Node.new(symbol, freq)
@@ -19,51 +19,37 @@ class Solution
       min_heap.insert(new_node)
     end
 
-    p print_codes(min_heap.extract)
+    min_heap.extract # return the root node of the huffman tree
   end
 
-  def print_codes(node, path = "", codes = {})
+  def get_codes(node, path = "", codes = {})
     if node.left.nil? && node.right.nil?
       codes[node.symbol] = path
     end
 
-    print_codes(node.left, path + "0", codes) if node.left
-    print_codes(node.right, path + "1", codes) if node.right
-
+    get_codes(node.left, path + "0", codes) if node.left
+    get_codes(node.right, path + "1", codes) if node.right
     codes
   end
 
+  def encode(input, codes)
+    input.each_char.map do |char|
+      codes[char]
+    end.join("")
+  end
+
   def decode(input)
-    nodes = []
-    input.each_char.tally.each do |symbol, freq|
-      nodes << Node.new(symbol, freq)
-    end
 
-    min_heap = MinHeap.new(nodes)
-    min_heap.build
-
-    while min_heap.size > 1
-      left = min_heap.extract
-      right = min_heap.extract
-      new_node = Node.new(left.symbol + right.symbol, left.freq + right.freq, left, right)
-
-      min_heap.insert(new_node)
-    end
   end
 end
 
 sol = Solution.new
-sol.encode("aaabccdeeeeeffg")
-puts "------------------------"
-sol.encode("aaabbb")
-puts "------------------------"
-sol.encode("aaabbccc")
+# sol.build_huffman_tree("aaabccdeeeeeffg")
+# puts "------------------------"
+# sol.build_huffman_tree("aaabbb")
+# puts "------------------------"
 
-# Output
-# a 000
-# g 0010
-# b 00110
-# d 00111
-# c 010
-# f 011
-# e 1
+root = sol.build_huffman_tree("aaabbccc")
+codes = sol.get_codes(root)
+sol.encode("aaabbccc", codes)
+p codes
