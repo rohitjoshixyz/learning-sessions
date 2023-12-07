cc3ebc691c6e6dae18cd29502a6eb17fd2b844e5
Rohit Joshi <rohit.joshiadvanced@gmail.com>
2023-12-08 02:06:50 +0530

    Advent of code day 4 part 2

--- ADVENT_OF_CODE_2023/day_04/game_2.rb
+++ ADVENT_OF_CODE_2023/day_04/game_2.rb
@@ -1,5 +1,6 @@
-card_counts = Hash.new(1)
-lines = File.readlines("ADVENT_OF_CODE_2023/day_04/test_input.txt", chomp: true)
+card_and_win_map = {}
+card_and_count_map = {}
+lines = File.readlines("ADVENT_OF_CODE_2023/day_04/input_1.txt", chomp: true)
 lines.each do |line|
   card, input = line.split(":")
   card_number = card.split(" ").last
@@ -7,18 +8,17 @@ lines.each do |line|
     str.strip.split(/\s+/)
   end
   count = winning_numbers.intersection(my_numbers).count
-  # 1: 2 3 4 5
-  # 2: 3 4 3 4
-  # 3: 4 5 4 5 4 5 4 5
-  # 4: 4
+  card_and_count_map[card_number.to_i] = 1
+  card_and_win_map[card_number.to_i] = count
 
-  i = 1
+end
 
-  while(i <= count) do
-    card_counts[card_number.to_i + i] += count
-    i += 1
+card_and_win_map.each do |card, win|
+  card_and_count_map[card].times do |i|
+    win.times do |j|
+      card_and_count_map[card + j + 1] += 1 if card_and_count_map[card + j + 1]
+    end
   end
 end
-p card_counts
-
+p card_and_count_map.values.sum
 # 7445 is low
