8fa3893f642126aee7d2a1d2f5d2900fdf7f7662
Rohit Joshi <rohit.joshiadvanced@gmail.com>
2023-12-08 14:08:15 +0530

    Advent of code day 3 part 1

--- ADVENT_OF_CODE_2023/day_03/puzzle_1.rb
+++ ADVENT_OF_CODE_2023/day_03/puzzle_1.rb
@@ -1,65 +1,44 @@
 
 line_length = File.readlines("ADVENT_OF_CODE_2023/day_03/test_input.txt", chomp: true).first.length
 lines = File.readlines("ADVENT_OF_CODE_2023/day_03/test_input.txt", chomp: true)
-grid = lines.map{|line| line.split("") }
+grid = lines.map { |line| line.split("").map { |char| ["&", "*", "=", "@", "$", "-", "+", "#", "/", "%"].include?(char) ? "#" : char } }
+# p grid.flatten.uniq.sort
 part_number_array = []
 
-padded_grid = []
-line_length.times { padded_grid << "." }
-grid.each do |row|
-  padded_grid << "."
-  padded_grid = padded_grid + row
-  padded_grid << "."
+def get_neighbours(x, y, grid)
+  neighbours = []
+  (-1..1).each do |i|
+    (-1..1).each do |j|
+      neighbours << grid[x + i][y + j] if (x + i) >= 0 && (y + j) >= 0 && (x + i) < grid.length && (y + j) < grid[0].length
+    end
+  end
+  neighbours
 end
-line_length.times { padded_grid << "." }
 
-# p padded_grid
-
-# grid_length = grid.length # 140 * 140
-# p line_length
 lines.each_with_index do |line, line_index|
-  numbers = line.split(/[\.&*=@$-+#\-\/]+/).reject(&:empty?)
-  symbols = line.split(/[\d\.]+/).reject(&:empty?)
+  number_and_index = []
 
-  numbers.each do |number|
+  line.scan(/[\d]+/) do |number|  
+    number_and_index << [number.to_i, Regexp.last_match.offset(0).first]
+  end
+  p number_and_index
+
+  number_and_index.each do |number, index|
     # puts "Number: #{number}, index: #{line.index(number) + (line_index * line_length)}"
-    number_index = line.index(number) + (line_index * line_length)
-    number_length = number.length
-
-    # i = 0
-    # while i < number_length + 2 do
-    #   j = 0
-    #   while j < 3 do
-    #     if (i == 0 || i == number_length + 2 || j == 0 || j == 2)
-    #       print "*"
-    #     else
-    #       print "-"
-    #     end
-    #     j += 1
-    #   end
-    #   puts ""
-    #   i += 1
-    # end
-
-
-    (-1..(number_length)).each do |i| # prev
-      [-1, 0, 1].each do |j|
-  
-        index_to_check = number_index + i + j * line_length
-        p index_to_check
-        # next if index_to_check < (line_index * line_length) || index_to_check > (line_index * line_length + line_length)
-
-
-          if symbols.include?(grid[index_to_check])
-            part_number_array << number
-          end
-          if symbols.include?(grid[index_to_check])
-            part_number_array << number
-          end
-        # end
-
+    number_index = index + (line_index * line_length)
+    number_length = number.to_s.length
+    "#{(number_index...(number_index + number_length))}"
+    (number_index...(number_index + number_length)).each do |i|
+      p "#{number_index} #{number_length} #{i} X=#{i / line_length} Y=#{i % line_length} #{number}"
+      if get_neighbours(i / line_length, i % line_length, grid).include?("#")
+        part_number_array << number
+        break
       end
     end
   end
 end
 p part_number_array.map(&:to_i).sum
+
+# 553156
+# 553087
+# 554003
